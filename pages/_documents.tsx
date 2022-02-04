1516addf891876ea3679c21cd1ea8b150c7a0cca
joseCorte-exe <josecorte-corte@hotmail.com>
2022-02-04 18:22:04 -0300

    :rocket: going to deploy

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -1,6 +1,8 @@
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+import { Html, Head, Main, NextScript } from 'next/document'
+
 export default class MyDocument extends Document {
 static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -30,13 +32,12 @@ static async getInitialProps(ctx) {
     render() {
         return (
           <Html>
-            <Head>
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link rel="preconnect" href="https://fonts.gstatic.com" />
-              <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-            </Head>
-            <Main />
-            <NextScript />
+            <Head />
+            <body>
+              <Main />
+              <NextScript />
+            </body>
           </Html>
         );
+    }
 }
