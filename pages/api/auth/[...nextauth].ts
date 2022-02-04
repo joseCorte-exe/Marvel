68de14042a0d2a3353da4f00e5da9362c5914b9e
joseCorte-exe <josecorte-corte@hotmail.com>
2022-02-04 18:48:12 -0300

    :rocket: going to deploy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,6 @@ export default NextAuth({
     GithubProvider({
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        scope: 'read:user'
     }),
     ],
 })
