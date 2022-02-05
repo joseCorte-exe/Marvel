a834cd25802a764dcbb03bdf012e82019fa1cbfc
joseCorte-exe <josecorte-corte@hotmail.com>
2022-02-04 21:55:39 -0300

    :bug:

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,9 +14,9 @@ export default function Header() {
         <Image src='/images/Marvel_Logo.svg' width={120} height={120} />
         <ul>
             <Link href={'/Comics/home'}  >Comics</Link>
-            <Link href={'/Series/home'} >Series</Link>
-            <Link href={'/Characters/home'} >Characters</Link>
-            <Link href={'/Events/home'} >Events</Link>
+            <Link href={'/series/home'} >Series</Link>
+            <Link href={'/characters/home'} >Characters</Link>
+            <Link href={'/events/home'} >Events</Link>
         </ul>
         <section>
             <article>
